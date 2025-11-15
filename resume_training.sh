bf7ae26829c33fb678ff9f53d4bf1c4414116c2e
namgyu1 <namgyu@postech.ac.kr>
2025-11-15 12:33:31 +0900

    resume 2 to 3

--- resume_training.sh
+++ resume_training.sh
@@ -8,11 +8,11 @@ DATA_PATH=../datasets/MSRVTT
 RPort=$(shuf -i 1000-9999 -n1)
 Margin=0.1
 beta=0.2
-CKPT_NAME=TAVIGATE
+CKPT_NAME=TAVIGATE_L2
 Tau=1.0
 
 # Set the epoch number to resume from (e.g., 0 for epoch 1, 1 for epoch 2, etc.)
-Resume_ep=0
+Resume_ep=1
 
 echo "Resuming training from epoch $Resume_ep"
 echo "Loading checkpoint from: ckpts/${CKPT_NAME}/pytorch_model.bin.${Resume_ep}"
